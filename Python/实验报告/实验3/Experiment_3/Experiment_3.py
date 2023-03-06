061053b6e22274ed97cb4e2b3e472cc5e35e409c
Simon <1224698515@qq.com>
2023-03-06 16:44:16 +0800

    Exercise 2

--- Python/实验报告/实验3/Experiment_3/Experiment_3.py
+++ Python/实验报告/实验3/Experiment_3/Experiment_3.py
@@ -1,11 +1,3 @@
-# main
-def main():
-    num = [0, 1, 2, 3, 4, 5]
-    printlist_a(num)
-    printlist_b(num)
-    print(f"len:{my_len(num)}")
-
-
 # Write a function that prints the elements of a list
 def printlist_a(num):
     for ele in num:
@@ -28,6 +20,29 @@ def my_len(num):
         return cnt
     else:
         return 0
+
+
+# set first element to zero
+def set_first_elem_to_zero(num):
+    num[0] = 0
+
+    return num
+
+
+# main
+def main():
+    num = [5050, 2, 3, 4, 5]
+
+    print(f"original list:{num}")
+    print(f"return list:{set_first_elem_to_zero(num)}")
+    print(f"original list:{num}")
+
+
+    # num = [0, 1, 2, 3, 4, 5]
+    # printlist_a(num)
+    # printlist_b(num)
+    # print(f"len:{my_len(num)}")
+
 
 
 if __name__ == '__main__':
